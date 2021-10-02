f5c1fc2afb1af56256e57b6fcea09df48ed2ccf3
AlexanderBowers <7319abowers@gmail.com>
2021-10-02 11:55:42 -0700

    show & create for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def show
+        user = current_user
+        render json: user
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+          # @user.save
+          token = encode_token({ user_id: @user.id })
+          render json: { user: UserSerializer.new(@user), jwt: token }, status: :created
+        else
+          render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+      end
+
 end
