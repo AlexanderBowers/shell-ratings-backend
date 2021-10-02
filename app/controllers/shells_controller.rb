612ae38be1ed7934f6959ebb70473744bc931084
AlexanderBowers <7319abowers@gmail.com>
2021-10-02 11:51:34 -0700

    index & show for shells controller

--- app/controllers/shells_controller.rb
+++ app/controllers/shells_controller.rb
@@ -1,2 +1,13 @@
 class ShellsController < ApplicationController
+
+    def index
+        shells = Shell.all
+        render json: shells
+    end
+
+    def show
+        shell = Shell.find_by(name: params[:name])
+        render json: shell
+    end
+
 end
